d8db679ee5b61d1e2f133899d22f2c307621511b
svarallo <svarallo@companieshouse.gov.uk>
2022-08-17 15:29:51 +0100

    Updating unit tests

--- test/next.five.working.day.calculator.spec.ts
+++ test/next.five.working.day.calculator.spec.ts
@@ -1,6 +1,6 @@
 import { getNextFiveWorkingDays } from "../src/next.five.working.day.calculator";
 import "mocha";
-import {expect} from "chai";
+import { expect } from "chai";
 
 const date: Date = new Date('2022-10-19');
 const dateBeforeChristmas: Date = new Date('2022-12-23');
@@ -13,12 +13,6 @@ describe('NEXT FIVE WORKING DAY CALCULATOR', () => {
     it('should return the next five working days', () => {
         const result = getNextFiveWorkingDays(date)
         expect(result).to.equal(mockFiveWorkingDays);
-    });
-
-    it('should throw error when given invalid date input', () => {
-    });
-
-    it('should throw error when given empty date input', () => {
     });
 
     it('should exclude Christmas day when calculating next five working days', () => {
